1461947f0421cf4bda58bab1ea145d648801c162
DanielLopez <d.lopez.torres@accenture.com>
2019-05-03 17:27:37 +0200

    [ADD] PART 3

---
+++ app/src/main/java/com/example/weathermvvm/ui/weather/detail/FutureDetailWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.weathermvvm.ui.weather.detail
+
+import androidx.lifecycle.ViewModel;
+
+class FutureDetailWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
