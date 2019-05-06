0d95b3c4520aafb0365ae9fd8950e69207550a36
DanielLopez <d.lopez.torres@accenture.com>
2019-05-06 15:12:25 +0200

    [ADD] DB

---
+++ app/src/main/java/com/example/weathermvvm/data/db/CurrentWeatherDao.kt
@@ -0,0 +1,23 @@
+package com.example.weathermvvm.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.weathermvvm.data.db.entity.CURRENT_WEATHER_ID
+import com.example.weathermvvm.data.db.entity.CurrentWeatherEntry
+import com.example.weathermvvm.data.db.unitlocalized.ImperialCurrentWeatherEntry
+import com.example.weathermvvm.data.db.unitlocalized.MetricCurrentWeatherEntry
+
+@Dao
+interface CurrentWeatherDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(weatherEntry: CurrentWeatherEntry)
+
+    @Query("SELECT * FROM current_weather WHERE id = $CURRENT_WEATHER_ID")
+    fun getWeatherMetric(): LiveData<MetricCurrentWeatherEntry>
+
+    @Query("SELECT * FROM current_weather WHERE id = $CURRENT_WEATHER_ID")
+    fun getWeatherImperial(): LiveData<ImperialCurrentWeatherEntry>
+}
